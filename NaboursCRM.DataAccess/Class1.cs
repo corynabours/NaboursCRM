70d46af4d49ac77f3c4d15bac94d8d3503335609
Cory Nabours <cnabours@virtualhold.com>
2011-07-22 14:42:29 -0400

    initial commit

---
+++ NaboursCRM.DataAccess/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NaboursCRM.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ NaboursCRM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NaboursCRM.Global" Language="C#" %>
